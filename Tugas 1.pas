f2e565399cf25ae7c9990c694f1d95fc8a1f9c80
hadli alansyah <haldialamsyah7@gmail.com>
2021-03-22 15:40:24 +0700

    tugas 1

---
+++ Tugas 1.pas
@@ -0,0 +1,18 @@
+program biodata;
+uses crt;
+
+begin
+write ('Nama = Haldi Alamsyah');
+readln;
+write ('Npm = 20.14.1.0036');
+readln;
+write ('Alamat = Desa Cibodas kec.Majalengka kab.Majalengka');
+readln;
+write ('TTL = Majelengka 12 November 2001');
+readln;
+write ('No.HP = 082113623673');
+readln;
+write ('Motto = Jangan Lupa Bersyukur');
+readln;
+
+end.
